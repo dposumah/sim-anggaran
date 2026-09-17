3259fcd49f9c8c741fb94d9bb14b0cefb4a2f24f
Donny Posumah <donny071289@gmail.com>
2026-09-17 11:26:08 +0800

    fix: next config for Next.js 15+

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['@prisma/client', 'prisma'],
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild/linux-x64',
+      'node_modules/typescript',
+      'node_modules/eslint',
+      'node_modules/prettier',
+      'node_modules/terser',
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '50mb'
-    },
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild/linux-x64',
-        'node_modules/typescript',
-        'node_modules/eslint',
-        'node_modules/prettier',
-        'node_modules/terser',
-      ],
     },
   }
 };
